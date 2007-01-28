9a0066b1f8b14c918edb5397c1babdc7268dd7d5
Bryan Donlan <bdonlan@gmail.com>
2007-01-28 08:03:14 +0100

    auto-lift KaosM operations

--- src/ASTToCore.hs
+++ src/ASTToCore.hs
@@ -16,14 +16,14 @@ emit x = tell [x]
 
 expToCore :: Expression Slot -> WriterT (CoreBlock Slot) KaosM Slot
 expToCore (EConst c) = do
-    s <- lift $ newSlot
+    s <- newSlot
     emit $ CoreConst s c
     return s
 expToCore (ELexical s) = return s
 expToCore (EBinaryOp op e1 e2) = do
     s1   <- expToCore e1
     s2   <- expToCore e2
-    dest <- lift $ newSlot
+    dest <- newSlot
     emit $ CoreAssign dest s1
     emit $ CoreLine [ TokenLiteral op
                     , TokenSlot (SA dest MutateAccess)
@@ -45,7 +45,7 @@ expToCore (ECall "print" [e]) = do
     return $ error "XXX: void return"
 expToCore (ECall "sqrt" [e]) = do
     s <- expToCore e
-    result <- lift $ newSlot
+    result <- newSlot
     emit $ CoreLine [ TokenLiteral "setv"
                     , TokenSlot (SA result WriteAccess)
                     , TokenLiteral "sqrt"
--- src/KaosM.hs
+++ src/KaosM.hs
@@ -4,13 +4,32 @@ import SeqT
 import Slot
 import Control.Monad.Identity
 import Control.Monad.Trans
+import Control.Monad.Reader
+import Control.Monad.Writer
+import Control.Monad.State
+
+class Monad m => MonadKaos m where
+    liftK :: KaosM a -> m a
+
+instance (MonadKaos m) => MonadKaos (StateT v m) where
+    liftK = lift . liftK
+
+instance (MonadKaos m) => MonadKaos (ReaderT a m) where
+    liftK = lift . liftK
+
+instance (Monoid w, MonadKaos m) => MonadKaos (WriterT w m) where
+    liftK = lift . liftK
+
+instance MonadKaos KaosM where
+    liftK = id
 
 newtype KaosM a = KM (SeqT Slot IO a)
     deriving (Monad, Functor)
 
-debugKM s = KM (lift $ putStrLn s)
-
-newSlot = KM getNext
+newSlot :: MonadKaos m => m Slot
+newSlot = liftK $ KM getNext
+debugKM :: MonadKaos m => String -> m ()
+debugKM s = liftK $ KM (lift $ putStrLn s)
 
 runKaosM (KM m) = runSeqT (Slot 0) m
 
